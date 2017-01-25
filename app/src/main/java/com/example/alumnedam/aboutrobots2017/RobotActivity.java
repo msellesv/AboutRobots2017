4758c808ca1fdda3c2180a9d6ecb3329c591d2b4
msellesv <marcselles.wm@gmail.com>
2017-01-25 19:13:57 +0100

    1

--- app/src/main/java/com/example/alumnedam/aboutrobots2017/RobotActivity.java
+++ app/src/main/java/com/example/alumnedam/aboutrobots2017/RobotActivity.java
@@ -62,6 +62,95 @@ public class RobotActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt("numeroVeces",numeroVeces);
+    }
+
 }
 
 
+
+
+/*
+package com.example.alumnedam.lifecycle;
+
+import java.util.Calendar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String fechaInicio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        notificar("onCreate");
+
+        if(savedInstanceState == null){
+
+            Calendar calendarHora = Calendar.getInstance();
+            int hora = calendarHora.get(Calendar.HOUR_OF_DAY);
+            int minuto = calendarHora.get(Calendar.MINUTE);
+            int segundo = calendarHora.get(Calendar.SECOND);
+            fechaInicio = "Aplicacion iniciada a las "+ hora +":"+ minuto +":"+segundo;
+
+        } else {
+
+            fechaInicio = savedInstanceState.getString("fechaHora");
+
+        }
+
+        TextView textView = (TextView) findViewById(R.id.textFecha);
+        textView.setText(fechaInicio);
+
+
+    }
+
+    protected void onStart(){
+        super.onStart();
+        notificar("onStart");
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+        notificar("onRestart");
+    }
+
+    protected void onResumne(){
+        super.onResume();
+        notificar("onResume");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        notificar("onPause");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        notificar("onStop");
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        notificar("onDestroy");
+    }
+
+    public void notificar(String frase){
+        Toast.makeText(this,frase,Toast.LENGTH_SHORT).show();
+        Log.i(frase,frase);
+    }
+
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putString("fechaHora",fechaInicio);
+    }
+}
+
+ */
